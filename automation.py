41f69fc20b116ba9b514fdfbee1ddecf73ddd545
Arthur <arthur.hoshino@gmail.com>
2022-09-09 10:57:18 -0400

    broadcaster column add

--- automation.py
+++ automation.py
@@ -9,8 +9,32 @@ directory = creds.user_path
 folderList = []
 fileList = []
 positiveList = []
+positiveListBrodcaster = []
 negativeList = []
+negativeListBrodcaster = []
 neutralList = []
+neutralListBrodcaster = []
+
+def addBroadcaster(list: list, broadcaster: str):
+    if broadcaster.startswith('SBT'):
+        list.append('SBT')
+    elif broadcaster.startswith('ALERTA SINOP'):
+        list.append('NOVATV')
+    elif broadcaster.startswith('BALANÇA SINOP'):
+        list.append('TV CIDADE VERDE')
+    elif broadcaster.startswith('BALANÇO GERAL'):
+        list.append('RECORD')
+    elif broadcaster.startswith('MT1'):
+        list.append('TVCA')
+    elif broadcaster.startswith('BOM DIA NORTÃO'):
+        list.append('TVCA')
+    elif broadcaster.startswith('CIDADE ALERTA'):
+        list.append('RECORD')
+    elif broadcaster.startswith('DE FRENTE COM A VERDADE'):
+        list.append('NOVATV')
+    else:
+        print('No broadscaster found')
+
 
 x = os.chdir(directory)
 folderList.append(os.listdir(x))
@@ -20,9 +44,6 @@ for a in folderList[0]:
 
     for b in os.listdir(y):
         fileList.append(b)
-
-# print(folderList)
-# print(fileList)
 
 # create Data-Frame
 data = pd.DataFrame({'Titles': fileList})
@@ -34,8 +55,6 @@ data.to_excel(toExcel, sheet_name='Titles', index=False)
 
 toExcel.save()
 
-
-# excelTable = pd.read_excel(creds.excel_path, sheet_name=['Titles', 'Positivo', 'Negattivo', 'Neutro'])
 excelTable = pd.read_excel(creds.excel_path)
 excelTable.reset_index()
 
@@ -43,15 +62,21 @@ for index, row in excelTable.iterrows():
     z = row['Titles'].strip()
 
     if z.startswith('1'):
-        positiveList.append(z.replace('1', '').strip().capitalize())
+        variable = z.replace('1', '', 1).strip()
+        positiveList.append(variable.replace('.txt', ''))
+        addBroadcaster(positiveListBrodcaster, variable)
     elif z.startswith('2'):
-        negativeList.append(z.replace('2', '').strip().capitalize())
+        variable = z.replace('2', '', 1).strip()
+        negativeList.append(variable.replace('.txt', ''))
+        addBroadcaster(negativeListBrodcaster, variable)
     else:
-        neutralList.append(z.replace('3', '').strip().capitalize())
+        variable = z.replace('3', '', 1).strip()
+        neutralList.append(variable.replace('.txt', ''))
+        addBroadcaster(neutralListBrodcaster, variable)
 
-positiveData = pd.DataFrame({'Positivo': positiveList})
-negativeData = pd.DataFrame({'Negativo': negativeList})
-neutralData = pd.DataFrame({'Neutro': neutralList})
+positiveData = pd.DataFrame({'Positivo': positiveList, 'Emissora': positiveListBrodcaster})
+negativeData = pd.DataFrame({'Negativo': negativeList, 'Emissora': negativeListBrodcaster})
+neutralData = pd.DataFrame({'Neutro': neutralList, 'Emissora': neutralListBrodcaster})
 
 with pd.ExcelWriter(creds.excel_path2) as writer:
     positiveData.to_excel(writer, sheet_name='Positivo', index=False)
